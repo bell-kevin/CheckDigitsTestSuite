7ac504896680d36db624d752fc661cd0fe34a000
JCBoyd <jcboyd@pm.me>
2020-03-17 14:29:14 -0600

    Version 1 (pre-testing)

---
+++ src/checkdigits/CheckDigits.java
@@ -0,0 +1,72 @@
+package checkdigits;
+
+/******************************************************************************
+* CheckDigits.java 
+* Programmer: @author jcboyd
+* Version: 1.0
+* Course: SDEV 2210
+* This program 
+******************************************************************************/
+
+import java.util.*; //Scanner
+
+public class CheckDigits {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        System.out.println("Project 9, Check Digits by JC Boyd\n");
+        double num1, num2, num3, num4;
+        
+        System.out.print("Enter first number: ");
+        num1 = Double.parseDouble(stdIn.nextLine());
+        System.out.print("Enter second number: ");
+        num2 = Double.parseDouble(stdIn.nextLine());
+        System.out.print("Enter third number: ");
+        num3 = Double.parseDouble(stdIn.nextLine());
+        System.out.print("Enter fourth number: ");
+        num4 = Double.parseDouble(stdIn.nextLine());
+    
+        System.out.printf("%10s%-7.2f\n%10s%-7.2f\n%10s%-7.2f\n%10s%-7.2f\n", 
+                "Total: ", getTotal(num1, num2, num3, num4),
+                "Average: ", getAverage(num1, num2, num3, num4),
+                "Largest: ", getLargest(num1, num2, num3, num4),
+                "Smallest: ", getSmallest(num1, num2, num3, num4));
+    } //end main
+    
+    public static double getTotal(double num1, double num2, double num3, double num4) {
+        return num1 + num2 + num3 + num4;
+    } //end getTotal
+    
+    public static double getAverage(double num1, double num2, double num3, double num4) {
+        return (num1 + num2 + num3 + num4) / 4;  
+    } //end getAverage
+    
+    public static double getLargest(double num1, double num2, double num3, double num4) {
+        double largest = num1;
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+        if (num4 > largest) {
+            largest = num4;
+        }           
+        return largest;
+    } //end getLargest 
+    
+      public static double getSmallest(double num1, double num2, double num3, double num4) {
+        double smallest = num1;
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        if (num4 < smallest) {
+            smallest = num4;
+        }           
+        return smallest;
+    } //end getSmallest  
+    
+} //end class CheckDigits
---
+++ test/checkdigits/CheckDigitsTest.java
@@ -0,0 +1,70 @@
+package checkdigits;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.runners.Parameterized.*;
+
+@RunWith(Parameterized.class)
+public class CheckDigitsTest {
+    
+    @Parameterized.Parameter(0)
+    public double num1;
+    @Parameterized.Parameter(1)
+    public double num2;
+    @Parameterized.Parameter(2)
+    public double num3;
+    @Parameterized.Parameter(3)
+    public double num4;
+    @Parameterized.Parameter(4)
+    public double largest;
+    @Parameterized.Parameter(5)
+    public double smallest;
+    
+    //test data for all methods
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+            {1.00, 2.00, 3.00, 4.00, 4.00, 1.00}, //test case 1  
+            {1.00, 2.00, 3.00, 4.00, 4.00, 1.00} //test case 2 
+        };
+        return Arrays.asList(data);
+    } //end Collection--data
+    
+    @Test
+    public void testGetTotal() {
+        System.out.println("getTotal");
+        double expResult = num1 + num2 + num3 + num4;
+        double result = CheckDigits.getTotal(num1, num2, num3, num4);
+        assertEquals(expResult, result, 0.001);
+    } //end testGetTotal
+
+    @Test
+    public void testGetAverage() {
+        System.out.println("getAverage");
+        double expResult = (num1 + num2 + num3 + num4) / 4;
+        double result = CheckDigits.getAverage(num1, num2, num3, num4);
+        assertEquals(expResult, result, 0.001);
+    } //end testGetAverage
+
+    @Test
+    public void testGetLargest() {
+        System.out.println("getLargest");
+        double expResult = largest;
+        double result = CheckDigits.getLargest(num1, num2, num3, num4);
+        assertEquals(expResult, result, 0.001);
+    } //end testGetLargest
+
+    @Test
+    public void testGetSmallest() {
+        System.out.println("getSmallest");
+        double expResult = smallest;
+        double result = CheckDigits.getSmallest(num1, num2, num3, num4);
+        assertEquals(expResult, result, 0.001);
+    } //end testGetSmallest
+    
+} //end class CheckDigitsTest
